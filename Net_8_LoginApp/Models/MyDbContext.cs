5c89dc870352ee6028ff97a8dc6ca168aef088e1
mamehteroglu <al20002009@windowslive.com>
2025-01-02 23:47:11 +0300

    c# ile yazdığım RESTFul API

---
+++ Net_8_LoginApp/Models/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Net_8_LoginApp.Models
+{
+    public class MyDbContext : DbContext
+    {
+      
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
